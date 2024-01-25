ea628214922d6638afd472091139039dd2ec9a13
Lavinia  Brito Copque <86345439517@unijorge.com.br>
2024-01-25 11:47:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Clientes from "./components/Clientes"
+import Header from "./components/Header"
+import Hero from "./components/hero"
+
+export default function App() {
+  return (
+    <main className="flex flex-col min-h-screen py-4">
+      <Header />
+      < Hero />
+     <Clientes />
+
+    </main>
+    
+  )
+}
